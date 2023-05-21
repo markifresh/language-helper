cadd796e7b5e219249072cd7d8ade026e266cbd9
Mark Shteiman <markifresh@gmail.com>
2023-05-21 20:05:27 +0300

    add class for TTS

--- audios.py
+++ audios.py
@@ -6,104 +6,68 @@ import requests
 load_dotenv()
 speach_key = getenv("MS_SPEACH")
 speach_region = getenv("MS_REGION")
-SERVICE_HOST = "customvoice.api.speech.microsoft.com"
 
 
-NAME = 'test_name'
-DESCRIPTION = 'test_DESCRIPTION'
-
-
-class MSAudio():
+class MSAudio:
     def __init__(self, api_key=speach_key, region=speach_region):
         self.api_key = api_key
         self.token = None
         self.region = region
-        self.api_version = 'v1.0'
+        self.api_ver_major = '1'
+        self.api_ver_minor = '0'
 
-
-    def make_request(self, method: str, url: str, params=None, headers=None):
+    def make_request(self, method: str, url: str, params=None, headers=None, data=None, json=None):
         method = method.lower()
-        api_url = f"https://{speach_region}.api.cognitive.microsoft.com/sts/{self.api_version}"
-        url = url[1:] if url.startswith('/') else url
-        url = f"{api_url}/{url}"
+
+        if 'https' not in url:
+            api_url = f'https://{self.region}.tts.speech.microsoft.com/cognitiveservices'
+            url = url[1:] if url.startswith('/') else url
+            url = f"{api_url}/{url}"
+
         if not headers:
             headers = {}
 
         headers['Ocp-Apim-Subscription-Key'] = self.api_key
         headers['Authorization'] = f'Bearer {self.token}'
 
-        result = getattr(requests, method)(url, headers=headers, params=params)
+        result = getattr(requests, method)(url, headers=headers, params=params, data=data, json=json)
         if result.status_code not in (200, ):
             raise Exception(result.status_code, result.text)
 
-        return result
+        return result.json() if result.encoding == 'utf-8' else result.text
 
-
-    def token_set(self):
-        url = '/issueToken'
+    def token_create(self):
+        api_version = f'v{self.api_ver_major}.{self.api_ver_minor}'
+        url = f'https://{self.region}.api.cognitive.microsoft.com/sts/{api_version}/issueToken'
         headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         return self.make_request('POST', url, headers=headers)
 
-
     def languages_get(self):
-        url = 'https://.tts.speech.microsoft.com/cognitiveservices/voices/list'
-        # "https://.tts.speech.microsoft.com/cognitiveservices/v1"
-        headers = {
-            'Ocp-Apim-Subscription-Key': speach_key
-        }
-        return requests.get(url, headers=headers)
+        url = '/voices/list'
+        return self.make_request('GET', url)
 
     def tts(self, text):
-        url = f"https://{speach_region}.tts.speech.microsoft.com/cognitiveservices/v1"
+        url = f'https://{self.region}.tts.speech.microsoft.com/cognitiveservices/v{self.api_ver_major}'
         headers = {
-            'Authorization': 'Bearer xxxx',
-            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
-            'Content-Type': 'application/ssml+xml',
-            'Ocp-Apim-Subscription-Key': 'xxxxx'
-        }
+                    'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+                    'Content-Type': 'application/ssml+xml',
+                  }
 
-        payload = "<speak version='1.0' xml:lang='en-US'>" + \
+        data = "<speak version='1.0' xml:lang='en-US'>" + \
                   "<voice xml:lang='en-US' xml:gender='Male'\r\n" + \
                   "name='en-US-ChristopherNeural'>\r\n" + \
                   f"{text}\r\n" + \
                   "</voice></speak>"
 
-        return requests.post(url, headers=headers, data=payload)
-
+        return self.make_request('POST', url, headers=headers, data=data)
+        # if response.status_code == 200:
+        #     with open("output.wav", "wb") as audio_file:
+        #         audio_file.write(response.content)
+        # else:
+        #     print("Error:", response.text)
 
     def synthesis_submit(self):
-        # url = f'https://{speach_region}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis'
-        url = f'https://{speach_region}.tts.speech.microsoft.com/cognitiveservices/v1'
-        header = {
-            'Ocp-Apim-Subscription-Key': speach_key,
-            'Content-Type': 'application/json'
-        }
-
-        payload = {
-            'displayName': NAME,
-            'description': DESCRIPTION,
-            "textType": "PlainText",
-            'synthesisConfig': {
-                "voice": "en-US-JennyNeural",
-            },
-            # Replace with your custom voice name and deployment ID if you want to use custom voice.
-            # Multiple voices are supported, the mixture of custom voices and platform voices is allowed.
-            # Invalid voice name or deployment ID will be rejected.
-            'customVoices': {
-                # "YOUR_CUSTOM_VOICE_NAME": "YOUR_CUSTOM_VOICE_ID"
-            },
-            "inputs": [
-                {
-                    "text": "here is my test"
-                },
-            ],
-            "properties": {
-                "outputFormat": "audio-24khz-160kbitrate-mono-mp3",
-                # "destinationContainerUrl": "<blob container url with SAS token>"
-            },
-        }
-
-        return requests.post(url, json=payload, headers=header)
+        pass
 
     def synthesis_wait(self):
         pass
