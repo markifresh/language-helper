e82af037fe7f4d5ed18e3454837cb05b2a1c44fa
Mark Shteiman <markifresh@gmail.com>
2023-06-20 20:14:51 +0300

    update GPT prompt

--- gpt_lang.py
+++ gpt_lang.py
@@ -6,21 +6,21 @@ from gpt_helper import send_question
 def create_request_fields(from_lang, to_lang, vocab_source, vocab_name, level='A1'):
     return {
         'id': '(id of word)',
-        'word': f'({from_lang} word without article)',
-        'whole_word': f'(if article exists for this word - {from_lang} word with article, else {from_lang} word)',
-        'word_translation': f'(translation to {to_lang})',
+        'word': 'word as it is',
+        'whole_word': f'(if article exists for this word - {from_lang} word with article, '
+                      f'if it is phrase or sentence - {from_lang} word)',
+        'word_translation': f'(translation of whole_word to {to_lang} language)',
         'sentence': f'(create sentence (which you think can be useful, it is up to you) '
-                    f'with {from_lang} word on {from_lang} (no need for translation here))',
-        'sentence_translation': f'(translation of sentence to {to_lang})',
-        'type':  f'(type of word: noun, verb, ..., but please use english language)',
-        'is_irregular': f'(is this word irregular according to {from_lang} grammar rules? (True or False))',
+                    f'with {from_lang} whole_word on {from_lang} (no need for translation here))',
+        'sentence_translation': f'(translation of sentence to {to_lang} language)',
+        'type':  f'(type of word: noun, verb, ... etc.)',
+        'is_irregular': f'(is this word irregular according to {from_lang} language grammar rules? (True or False))',
         'level':   f'{level} if {level} else please define a level for this word/sentence from A1 to C2',
         'language': f'(full language name of {from_lang.lower()} in lowercase in english)',
         'to_language': f'(full language name of  {to_lang.lower()} in lowercase in english)',
         'topics':  f'(coma+space separated list of topics to which this word can be related. '
-                   f'Can be few topics at the same time f.e. "weather, time".'
-                   f'Please use only english language for this field)',
-        'source':  f'("{vocab_source}, {vocab_name}" for all)'
+                   f'Can be few topics at the same time f.e. "weather, time".)',
+        'source':  f'(use this string for all words: "{vocab_source}, {vocab_name}")'
         }
 
 
@@ -29,10 +29,10 @@ def create_request_message(from_lang, to_lang, vocab_source, vocab_name, level):
     to_lang = to_lang.title()
     fields = create_request_fields(from_lang, to_lang, vocab_source, vocab_name, level)
     fields_str = "\n".join([f'- {field} {fields[field]}' for field in fields])
-    return f'I learn {from_lang}, but you please speak English. Please make a list of dictionaries (json). ' \
+    return f'I learn {from_lang}. Please make a list of dictionaries (json).' \
            'Each dictionary should have keys: \n' \
            f'{fields_str} \n' \
-           '(your reply should be in json format)' \
+           f'(your reply should be in json format and should include {to_lang} language)' \
            f'To fill this list please use the following list of {from_lang} words: \n'
 
 
